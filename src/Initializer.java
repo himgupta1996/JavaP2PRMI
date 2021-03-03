57fa2cc6012ec9402e262ead1adc34bff53a0759
Himanshu Gupta <himanshu.gupta@citrix.com>
2021-03-03 10:00:25 -0500

    Added the testcases and Logger Functionality.

--- src/Initializer.java
+++ src/Initializer.java
@@ -1,9 +1,38 @@
 import java.util.*;
 import java.rmi.registry.LocateRegistry; 
 import java.rmi.registry.Registry;  
+import java.util.logging.*;
 
 public class Initializer {
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("MyLog"); 
+	    FileHandler fh;  
+		int testcase = 3;
+		String filename;
+		if(testcase ==1) {
+			filename = "testcase1.log";
+		}
+		else if(testcase ==2) {
+			filename = "testcase2.log";
+		}
+		else if(testcase ==3) {
+			filename = "testcase3.log";
+		}
+		else {
+			filename = "P2P.log";
+		}
+		try {
+			
+			fh = new FileHandler(filename); 
+			logger.addHandler(fh);
+	        SimpleFormatter formatter = new SimpleFormatter();  
+	        fh.setFormatter(new MyFormatter()); 
+	        logger.info("My first log");  
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();  
+		}
 		
 		//TODO:configurable parameter..can be taken as input		
 		int N = 2;
@@ -34,14 +63,13 @@ public class Initializer {
 		String peer2item = "Boar";
 		
 		//if input comes, initialize with that value, otherwise 3
-		int testcase = 3;
 		
 		//Randomly initialize the items in case args not passed
 		
 		//Initialize all the peers
 		//TODO: get the associated port and ipaddress
-		Node peer1 = new Node(1, "buyer", 8003, peer1item, network.get_associated_peer(1), testcase);
-		Node peer2 = new Node(2, "seller", 8004, peer2item, network.get_associated_peer(2), testcase);
+		Node peer1 = new Node(1, "buyer", 8003, peer1item, network.get_associated_peer(1), testcase, logger);
+		Node peer2 = new Node(2, "seller", 8004, peer2item, network.get_associated_peer(2), testcase, logger);
 		
 		//TODO:Check which is buyer, only call buyer.start
 		peer1.start();
--- src/Node.java
+++ src/Node.java
@@ -2,25 +2,29 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
-
+import java.util.logging.*;
 public class Node implements Hello {
 	public String role;
 	public int node_id;
 	public int port_id;
+	private int max_sell_items = 5;
 	private int m=5;
 	private String item;
 	String items[] = {"Boar", "Salt", "Fish"};
 	private int[] peers;
 	private int sellerId = -1;
 	private int testcase;
+	private Logger logger;
+	private int hopcount = 1;
 	
-	public Node(int node_id, String role,  int port_id, String item, int[] peers, int testcase) {
+	public Node(int node_id, String role,  int port_id, String item, int[] peers, int testcase, Logger logger) {
 		this.role = role;
 		this.node_id = node_id;
 		this.port_id = port_id;
 		this.item = item;
 		this.peers = peers;
 		this.testcase = testcase;
+		this.logger = logger;
 		try { 	    
 	         // Exporting the object of implementation class  
 	         // (here we are exporting the remote object to the stub) 
@@ -30,6 +34,7 @@ public class Node implements Hello {
 	         
 	         registry.bind(String.valueOf(this.node_id), stub);  
 	         System.out.println("Peer "+this.node_id+" ready"); 
+	         logger.info("Peer "+this.node_id+" ready");
 	    } 
 		catch (Exception e) { 
 	         System.err.println("Peer exception: " + e.toString()); 
@@ -39,11 +44,6 @@ public class Node implements Hello {
 	
 	public void lookup_helper(String productname, int hopcount) {
 		System.out.println("Peer:"+this.node_id+" In lookup_helper");
-//		System.out.println("My item is "+this.item);
-//		System.out.println("The product name is: "+ productname);
-//		System.out.println("The hpcount is "+ hopcount );
-		
-//		System.out.println("My role is "+this.role);
 		
 		System.out.println("I have "+this.m+" items of "+this.item);
 		
@@ -51,13 +51,29 @@ public class Node implements Hello {
 			if(productname.equals(this.item)) {
 				if(this.m>0) {
 					//TODO: remove the buyer Id hard coding
+					logger.info("Peer:"+this.node_id+": I have "+this.m+" items of '"+this.item+"'");
 					this.reply(1, this.node_id);
 				}
 				else if(this.m == 0) {
-					System.out.println("Peer:"+this.node_id+" My items are finished. Restocking them.");
+					System.out.println("Peer:"+this.node_id+": My items are finished. Restocking them.");
+					logger.info("Peer:"+this.node_id+": My items are finished. Restocking them.");
 					//TODO: Remove the hardcoded m
-					this.m = 5;
+					this.m = max_sell_items;
+					if(this.testcase==1) {
+						this.item = "Fish";
+					}
+					else if (this.testcase == 2) {
+						this.item = "Boar";
+					}
+					else {
+						int rnd = new Random().nextInt(this.items.length);
+					    this.item = this.items[rnd];		
+					}						
 				}
+			}
+			else {
+				logger.info("Peer: "+this.node_id+": I don't have '"+productname+"'. I have '"+this.item+"'");
+				//TODO:This should further send to the network except from where it came
 			}
 		}
 		else {
@@ -72,16 +88,19 @@ public class Node implements Hello {
 		    this.m-=1;
 	    }
 	    else {
-	    	System.out.println("I am peer "+ this.node_id + ". I found my seller in peer "+ sellerId);
-	    	System.out.println("Let me now buy it");
+	    	System.out.println("Peer: "+ this.node_id + ": I found my seller in peer "+ sellerId);
+	    	System.out.println("Going to buy it");
+	    	logger.info("Peer: "+ this.node_id + ": I found my seller in peer "+ sellerId);
 	    	try {
+	    		logger.info("Peer: "+ this.node_id +": Buying '"+this.item+ "' from peer "+sellerId);
 	    		//TODO: get the seller port Id and Ip address from the configuration file
 	    		String neighbour_ip = "127.0.0.1";
 				int neighbour_port = 8004;
 				Registry registry = LocateRegistry.getRegistry(neighbour_ip, neighbour_port); 
 				Hello stub = (Hello) registry.lookup(String.valueOf(sellerId));
 				stub.buy(this.node_id);	
-				System.out.println("Successfully completed the transaction");
+				System.out.println("Peer: "+ this.node_id +": Successfully completed the transaction");
+				logger.info("Peer: "+this.node_id+ ": Successfully bought '"+ this.item + "' from "+sellerId);
 				this.sellerId = -1;
 			}
 			catch(Exception e) {
@@ -104,6 +123,7 @@ public class Node implements Hello {
 	
 	private void lookup(String product_name, int hopcount) {
 		System.out.println("Peer:"+this.node_id+" In lookup");
+		logger.info("Peer: "+this.node_id+": Looking up '"+product_name+ "' in my neighbour peers.");
 		if(hopcount>0) {
 			for(int i= 0; i<this.peers.length;i++) {
 				int neighbour_peer = peers[i];
@@ -145,17 +165,26 @@ public class Node implements Hello {
 	public void start() {
 		if(this.role == "buyer"){
 			int i = 0;
-			while(i!=8){
-//				if (this.item == "") {
-//					int rnd = new Random().nextInt(this.items.length);
-//				    this.item = this.items[rnd];
-//				}
+			while(i!=8){			
+				if(this.testcase == 1 || this.testcase == 2) {
+					this.item = "Fish";
+				}
+				else {
+					int rnd = new Random().nextInt(this.items.length);
+				    this.item = this.items[rnd];
+				}
+
 //				//TODO: take the variable hopcount as input and check if it is greater than zero..If yes, call lookup
 //				//TODO: remove the hardcoding
 //				this.item = "Salt";
-				this.lookup(this.item, this.node_id);
+			    System.out.println("Peer: "+this.node_id+ ": I want to buy "+ this.item);
+			    logger.info("Peer: "+this.node_id+ ": I want to buy '"+ this.item+"'");
+				this.lookup(this.item, this.hopcount);
 				if(this.sellerId !=-1) {
 					this.buy(sellerId);
+				}
+				else {
+					logger.info("Peer: "+this.node_id+ ": Couldn't find the item '"+ this.item+"'");
 				}
 				i+=1;
 			}
